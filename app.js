42e92b36c027cd991ed1e919d66c672740f1096a
Joe Bigio <jvbigio@gmail.com>
2020-08-10 15:28:45 -0400

    Changes to html/css

--- app.js
+++ app.js
@@ -1,3 +1,60 @@
 const result = document.querySelector('.result')
+const addBtn = document.querySelector('.add').addEventListener('click', add)
+const numberKeys = document.querySelectorAll('.number')
 
-// result.innerHTML = 
+numberKeys.forEach(number => {
+  number.addEventListener('click', _ => {
+    result.innerHTML += number.textContent
+  })
+})
+
+// numberKeys.addEventListener('keydown', (e) => {
+//   // console.log(`Key ${e.key} pressed`)
+//   // result.innerHTML = 7 // works
+//   result.innerHTML = e.key
+// })
+
+// MATH OPERATIONS //
+function add (...numbers) {
+  return numbers.reduce((previous, current) => {
+    // result.innerHTML = (previous + current)
+    // return previous + current
+    const sum = previous + current
+    document.querySelector('.result').innerHTML = sum
+  })
+}
+
+// function subtract () {
+//   // Add code
+// }
+
+// function multiply () {
+//   // add code
+// }
+
+// function divide () {
+//   // add code
+// }
+
+// // Memory Keys //
+// function deleteAll () {
+//   // add code
+// }
+
+// function memoryClear () {
+//   // add code
+// }
+
+// function memoryRecall () {
+//   // add code
+// }
+
+// function memorySave () {
+//   // add code
+// }
+
+/*
+NOTES:
+
+Add functionality for sqr root, +/-, and percentage for future project
+*/
