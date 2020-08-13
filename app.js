e536f05c88cc2fd29c7a13fffaf306fcc5e7a89c
Joe Bigio <jvbigio@gmail.com>
2020-08-13 10:46:40 -0400

    Working on calc functions

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
 const result = document.querySelector('.result')
-const addBtn = document.querySelector('.add').addEventListener('click', add)
 const numberKeys = document.querySelectorAll('.number')
+document.querySelector('.add').addEventListener('click', add)
+// const operationsArray = [].slice.call(document.querySelectorAll('.operation'))
+const operations = document.querySelectorAll('.operation')
+const mathOperators = ['+', '-', '*', '/']
 
+// listen for math operator click
+operations.forEach(operator => {
+  return operator.addEventListener('click', e => {
+    if (e.target.innerHTML === '+') {
+      // Cannot get keyCode to work:
+      // if (e.keyCode === 187) {
+      // console.log('clicked')
+      add()
+    } else if (e.target.innerHTML === '-') {
+      subtract()
+    } else if (e.target.innerHTML === '/') {
+      divide()
+    } else if (e.target.innerHTML === '*') {
+      multiply()
+    }
+  })
+})
+
+// numberKeys.forEach(number => console.log(number.innerHTML))
+
+// list for number key presses and display on calculator
 numberKeys.forEach(number => {
   number.addEventListener('click', _ => {
     result.innerHTML += number.textContent
   })
 })
 
-// numberKeys.addEventListener('keydown', (e) => {
-//   // console.log(`Key ${e.key} pressed`)
-//   // result.innerHTML = 7 // works
-//   result.innerHTML = e.key
-// })
-
 // MATH OPERATIONS //
-function add (...numbers) {
-  return numbers.reduce((previous, current) => {
-    // result.innerHTML = (previous + current)
-    // return previous + current
-    const sum = previous + current
-    document.querySelector('.result').innerHTML = sum
-  })
+/*
+pseudo code:
+1) capture first number and save it to a variable
+2) check if there's an operator currently in place
+3) capture second number and save it to a variable
+4) equal performs the computation between first and second number
+*/
+// function add (...numbers) {
+//   return numbers.reduce((previous, current) => {
+//     const sum = parseInt(previous) + parseInt(current)
+//     // display sum to calc screen
+//     document.querySelector('.result').innerHTML = sum
+//     return sum
+//   }, 0)
+// }
+
+
+// console.log(add(5, 5))
+
+function add () {
+  const num1 = NumOne()
+  const num2 = NumTwo()
+
+  const sum = parseInt(num1) + parseInt(num2)
+  result.innerHTML += sum
 }
+
+function NumOne () {
+  document.in
+}
+
+function NumTwo () {
+  return innerHTML.value
+}
+
+console.log(add(1, 2, 3))
 
 // function subtract () {
 //   // Add code
