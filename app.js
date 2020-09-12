67d1ecacbf8fb94690bfc6b0f2f73beb1ea00eea
Joe Bigio <jvbigio@gmail.com>
2020-09-12 13:28:05 -0400

    refactored

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
 const operations = document.querySelectorAll('.operation')
 const result = document.querySelector('.result')
 
-/*
-MAYBE DELETE numKeys eventListener and move everything into buttons eventListener?
-
-Then check if the className is a number or math operator.
-If number, capture and push into array AFTER an operator is pushed?
-*/
-
-const numberKeys = document.querySelectorAll('.number')
-numberKeys.forEach(numKeys => {
-  numKeys.addEventListener('click', e => {
-    const savedNumbers = []
-    let num1, num2
-    const numbers = parseInt(e.target.textContent)
-    // console.log(numbers)
-    // console.log(parseInt(e.target.textContent))
-    result.innerHTML += numbers
-    // console.log(numbers)
-  })
-})
 const buttons = document.querySelector('.buttons-container')
 buttons.addEventListener('click', e => {
+  const savedNumbers = []
+  let num1, num2
+  const numbers = parseInt(e.target.textContent)
+  // console.log(numbers)
+  // console.log(parseInt(e.target.textContent))
+  result.innerHTML += numbers
+  // console.log(numbers)
   const button = e.target
   // console.log(button)
   if (button.classList.contains('number')) {
+    num1 = parseInt(button.textContent)
+    console.log(num1)
+  }
+  if (button.classList.contains('operation')) {
+    console.log(num1)
     console.log(button.textContent)
+    savedNumbers.push(num1)
+    console.log(savedNumbers)
   }
 })
 
 function calculator (num1, operator, num2) {
-  const result = []
+  const savedNums = []
+  let sum
   if (operator === '*') {
     // return num1 * num2
   } else if (operator === '/') {
     // return num1 / num2
   } else if (operator === '+') {
     // return num1 + num2
-    result.push(num1)
-    console.log(num1)
-    result.push(num2)
-    console.log(num2)
-    const sum = result.reduce((a, b) => a + b, 0)
-    console.log(sum)
+    savedNums.push(num1)
+    // console.log(num1)
+    savedNums.push(num2)
+    // console.log(num2)
+    sum = savedNums.reduce((a, b) => a + b, 0)
+    // console.log(sum)
   } else if (operator === '-') {
     // return num1 - num2
   }
+  return sum
 }
-console.log(calculator(50, '+', 1))
+// console.log(calculator(50, '+', 1))
