9efec8514e15da7d0a5c9a025d8500e4a30ea2c1
Joe Bigio <jvbigio@gmail.com>
2020-09-11 20:15:27 -0400

    cleaned up code

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
+// MATH OPERATIONS //
+/*
+pseudo code:
+1) capture first number and save it to a variable
+2) check if there's an operator currently in place
+3) capture second number and save it to a variable
+4) equal performs the computation between first and second number
+*/
+
 document.addEventListener('DOMContentLoaded', function () {
-  // runCalculator()
-  // calculator()
+  runCalculator()
 })
 
-let result = document.querySelector('.result')
+const result = document.querySelector('.result')
 const numberKeys = document.querySelectorAll('.number')
 numberKeys.forEach(numKeys => {
   numKeys.addEventListener('click', e => {
@@ -16,20 +24,8 @@ numberKeys.forEach(numKeys => {
 const buttons = document.querySelector('.buttons-container')
 buttons.addEventListener('click', e => {
   const button = e.target
-  console.log(button)
- 
+  // console.log(button)
 })
-
-// console.log(mathOperations)
-
-// numberKeys.forEach(numKeys => {
-//   numKeys.addEventListener('click', e => {
-//     // console.log(typeof e.target.textContent) // string
-//     // console.log(parseInt(e.target.textContent)) // number
-//     // renders numbers on calculator display screen:
-//     result.innerHTML += numKeys.textContent
-//   })
-// })
 
 // operations.forEach(operator => operator.addEventListener('click', e => {
 //   if (operator.textContent === '=') {
@@ -38,15 +34,7 @@ buttons.addEventListener('click', e => {
 //   }
 // }))
 
-// MATH OPERATIONS //
-/*
-pseudo code:
-1) capture first number and save it to a variable
-2) check if there's an operator currently in place
-3) capture second number and save it to a variable
-4) equal performs the computation between first and second number
-*/
-
+// run only if = sign pressed
 // function runCalculator () {
 //   operations.forEach(operator => operator.addEventListener('click', e => {
 //     if (operator.textContent === '=') {
@@ -55,24 +43,9 @@ pseudo code:
 //   }))
 // }
 
-// function calculator (num1, operator, num2) {
-//   operations.forEach(operator => operator.addEventListener('click', e => {
-//     // console.log(operator.textContent)
-//     numberKeys.forEach(numKeys => {
-//       numKeys.addEventListener('click', e => {
-//         result.innerHTML += numKeys.textContent
-//         console.log(operator.textContent)
-//       })
-//     })
-//   }))
-// }
-// // console.log(calculator(5, '*', 5))
-// console.log(calculator(5, '/', 5))
-// console.log(calculator(5, '+', 5))
-// console.log(calculator(5, '-', 5)
-
 function runCalculator (num1, operator, num2) {
   if (operator === '*') {
+    // console.log(num1, num2) // 5, 2
     return parseInt(num1) * parseInt(num2)
   } else if (operator === '/') {
     return parseInt(num1) / parseInt(num2)
@@ -82,3 +55,4 @@ function runCalculator (num1, operator, num2) {
     return parseInt(num1) - parseInt(num2)
   }
 }
+// console.log(runCalculator(5, '*', 2)) // 10
