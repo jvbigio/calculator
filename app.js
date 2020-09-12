003acb529b2b65a323550622dd539db91485a0ef
Joe Bigio <jvbigio@gmail.com>
2020-09-12 19:22:55 -0400

    test case 1-3 works

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
-const operations = document.querySelectorAll('.operation')
+// const operations = document.querySelectorAll('.operation')
 const result = document.querySelector('.result')
+let num1, num2, operation
+const savedNumbers = []
 
 const buttons = document.querySelector('.buttons-container')
 buttons.addEventListener('click', e => {
-  const savedNumbers = []
-  let num1, num2
-  const numbers = parseInt(e.target.textContent)
-  // console.log(numbers)
-  // console.log(parseInt(e.target.textContent))
-  result.innerHTML += numbers
-  // console.log(numbers)
+  const numbers = parseInt(e.target.textContent) // original
+  // const numbers = e.target.textContent
+
   const button = e.target
-  // console.log(button)
   if (button.classList.contains('number')) {
-    num1 = parseInt(button.textContent)
+    result.innerHTML += numbers
+    num1 = parseInt(button.textContent) // original
+    // num1 = button.textContent
     console.log(num1)
-  }
-  if (button.classList.contains('operation')) {
-    console.log(num1)
-    console.log(button.textContent)
+    result.innerHTML = num1
+
+    // console.log(num2)
+    // savedNumbers.push(num1)
+    // console.log(savedNumbers)
+  } else if (button.classList.contains('operation')) {
+    console.log('operation button was pressed!!')
     savedNumbers.push(num1)
+    result.innerHTML = num1
+    // result.innerHTML = ''
+
+
+    // num2 = (parseInt(button.textContent))
+    // // console.log(num2)
+    // console.log(button.textContent)
+    // savedNumbers.push(num1)
     console.log(savedNumbers)
   }
 })
