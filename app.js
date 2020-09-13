75c76c508e1393ce30e20d70d4f4e95928ecc2bc
Joe Bigio <jvbigio@gmail.com>
2020-09-13 12:46:52 -0400

    Working on test case 1

--- app.js
+++ app.js
@@ -1,45 +1,35 @@
 // const operations = document.querySelectorAll('.operation')
 const result = document.querySelector('.result')
-// operation equal to whatever operation was pressed
 let num1, num2, operation
 const savedNumbers = []
 
 const buttons = document.querySelector('.buttons-container')
 buttons.addEventListener('click', e => {
-  // const btnPressed = parseInt(e.target.textContent) // original
-  // don't parseInt yet, bc not all keys will be numbers here
-  const btnPressed = e.target.textContent
-  console.log(btnPressed)
-  const isNumberOrOperator = e.target
-  console.log(isNumberOrOperator)
+  const btnPressed = e.target.textContent // innerText of btn pressed
+  const isNumberOrOperator = e.target // ex: <button class="number"</button>
 
   if (isNumberOrOperator.classList.contains('number')) {
-    // result.innerHTML += btnPressed // original
+    result.innerHTML += btnPressed // original
     // append result when an operation is pushed, check to see if u update screen with current button pressed or add onto it happens on line below:
-    result.innerHTML += num1
+    // result.innerHTML += num1
     num1 = parseInt(isNumberOrOperator.textContent) // original
-    // num1 = isNumberOrOperator.textContent
-    console.log(num1)
-    result.innerHTML = num1
-
-    // console.log(num2)
-    // savedNumbers.push(num1)
-    // console.log(savedNumbers)
+    console.log(num1) // 1st number pressed
+    // result.innerHTML = num1 // this is actually 2nd
   } else if (isNumberOrOperator.classList.contains('operation')) {
     console.log('operation button was pressed!!')
-    savedNumbers.push(num1)
+    // savedNumbers.push(num1) // original
+    num1 = savedNumbers.push(num1) // original
     result.innerHTML = num1
-    // result.innerHTML = ''
-
-    // num2 = (parseInt(isNumberOrOperator.textContent))
-    // // console.log(num2)
-    // console.log(isNumberOrOperator.textContent)
+    operation = isNumberOrOperator.textContent
+    // console.log(operation) // +
     // savedNumbers.push(num1)
-    console.log(savedNumbers)
-
-    // if '=' sign pressed:
-    // try this:
-    // calculator(num1, operator, num2)
+    if (isNumberOrOperator.textContent === '=') {
+      console.log('equal sign pressed') // works
+      console.log(savedNumbers)
+      console.log(operation)
+      // console.log(num1, operation, num2)
+      // calculator(num1, operation, num2)
+    }
   }
 })
 
@@ -53,14 +43,16 @@ function calculator (num1, operator, num2) {
   } else if (operator === '+') {
     // return num1 + num2
     saved.push(num1)
-    // console.log(num1)
+    console.log(num1)
     saved.push(num2)
-    // console.log(num2)
-    sum = saved.reduce((a, b) => a + b, 0)
+    console.log(num2)
+    // sum = saved.reduce((a, b) => a + b, 0)
+    sum = saved.reduce((num1, num2) => num1 + num2, 0)
     // console.log(sum)
   } else if (operator === '-') {
     // return num1 - num2
   }
   return sum
 }
+// console.log(calculator)
 // console.log(calculator(50, '+', 1))
