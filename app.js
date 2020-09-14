de3012304197570c641e8ecde80d56aa6fd0504d
Joe Bigio <jvbigio@gmail.com>
2020-09-14 13:56:00 -0400

    refactored

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
 const result = document.querySelector('.result')
 let num1, num2, operation
-const savedNumbers = []
+const inputedNumbers = []
+let isFirstKeyPressed = true
+const outputedResult = ''
 
 const buttons = document.querySelector('.buttons-container')
 buttons.addEventListener('click', getInput)
 
 function getInput (e) {
-  const btnPressed = e.target.textContent
-  const isNumberOrOperator = e.target
-
-  if (isNumberOrOperator.classList.contains('number')) {
-    result.innerHTML = btnPressed
-    num1 = parseInt(isNumberOrOperator.textContent)
-  } else if (isNumberOrOperator.classList.contains('operation')) {
-    console.log('operation button was pressed!!')
-    num1 = savedNumbers.push(num1)
-    result.innerHTML = num1
-    if (isNumberOrOperator.textContent !== '=') {
-      operation = isNumberOrOperator.textContent
+  const keyPressed = e.target
+  // const outputResult = result.textContent
+  // console.log(outputResult)
+  // console.log(keyPressed)
+  if (keyPressed.classList.contains('number')) {
+    if (isFirstKeyPressed) {
+      num1 = e.target.textContent
+      // inputedNumbers.push(num1)
+      result.innerHTML = num1
+    } else {
+      num2 = e.target.textContent
+      result.innerHTML = num2
     }
-
-    if (isNumberOrOperator.textContent === '=') {
-      console.log('equal sign pressed')
-      num1 = savedNumbers[0] // 1
-      num2 = savedNumbers[1] // 2
+  } else if (keyPressed.classList.contains('operation')) {
+    isFirstKeyPressed = false
+    if (keyPressed.textContent !== '=') {
+      operation = keyPressed.textContent
+    } else if (keyPressed.textContent === '=') {
       calculator(num1, operation, num2)
     }
   }
 }
 
 function calculator (num1, operator, num2) {
-  const saved = []
-  let answer
+  let computedValue
+
   if (operator === '*') {
-    answer = num1 * num2
+    return
   } else if (operator === '/') {
-    answer = num1 / num2
+    return
   } else if (operator === '+') {
-    answer = num1 + num2
-    saved.push(answer)
+    computedValue = parseFloat(num1) + parseFloat(num2)
   } else if (operator === '-') {
-    answer = num1 - num2
+    return
   }
-  result.innerHTML = answer // 3
-  console.log(answer) // 3!!!! YAY
-  console.log(saved) // [3]
-  return saved
-  // return answer
+  renderAnswer(computedValue)
 }
-// console.log(calculator(50, '+', 1))
+
+function renderAnswer (answer) {
+  result.innerHTML = answer
+}
