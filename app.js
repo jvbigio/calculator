7d0fe67433f038b671b955de03375e9476dc5f87
Joe Bigio <jvbigio@gmail.com>
2020-09-15 18:31:55 -0400

    refactored, using objects

--- app.js
+++ app.js
@@ -1,87 +1,84 @@
-const result = document.querySelector('.result')
+// const result = document.querySelector('.result')
+document.addEventListener('DOMContentLoaded', function () {
+  // renderScreen()
+  // captureDigits()
+})
+
 const calcDisplay = document.querySelector('.calculator-display')
-let num1, num2, operation
-let isFirstKeyPressed = true
+let firstNumberSet, secondNumberSet, operation
+let isOperatorPressed = false
 const buttons = document.querySelector('.buttons-container')
-buttons.addEventListener('click', getInput)
-const savedNumbers = []
+buttons.addEventListener('click', getInputType)
 
-// [1,2,3,4,5].reduce((acc, current)=>acc+current, 0)
-
-function getInput (e) {
-  const keyPressed = e.target
-  let numberTracker = result.innerHTML
-  console.log(numberTracker) // 1122
-  console.log(keyPressed)
-  if (keyPressed.classList.contains('number')) {
-    if (isFirstKeyPressed) {
-      // num1 = e.target.textContent * 10 // orig
-      num1 = e.target.textContent
-      // num1 = e.target.value // 1 for key 1
-      console.log(num1)
-
-      // const tempNumber = number
-      // console.log(tempNumber) // 11
-      // num1 = parseFloat(num1) + parseFloat(number) // orig
-      // result.innerHTML += number
-      result.innerHTML += num1
-      num1 += num1
-      console.log(num1) // 11
-      savedNumbers.push(num1) // string 11
-    } else {
-      // console.log(savedNumbers) // blank
-      // result.innerHTML = ''
-      num2 = e.target.textContent
-      // console.log(num2)
-      // result.innerHTML += number
-      // num2 = e.target.textContent * 10
-      // num2 = parseFloat(num2) + parseFloat(number)
-      console.log(num2) // 2
-      savedNumbers.push(num2)
-      // console.log(savedNumbers)
-      // result.innerHTML += num2 // keep
-      // display correct but only same dbl digits: 11, 22, 33, etc
-      result.innerHTML = numberTracker[1]
-      console.log(keyPressed.innerHTML) // 2
-      result.innerHTML += keyPressed.innerHTML
-      num2 += num2
-    }
-  } else if (keyPressed.classList.contains('operation')) {
-    isFirstKeyPressed = false
-    // num1 = '' // works but erases 11
-    console.log(num1)
-    
-    savedNumbers.push(num1)
-    console.log(savedNumbers)
-    // result.innerHTML = savedNumbers[0]
-
-    if (keyPressed.textContent !== '=') {
-      operation = keyPressed.textContent
-    } else if (keyPressed.textContent === '=') {
-      console.log(num1, operation, num2)
-      calculator(num1, operation, num2)
-    }
-    // numberTracker = ''
-  }
+const calculator = {
+  displayOutput: null,
+  num1: null,
+  num2: null,
+  operator: null
 }
 
-// console.log('1' + '1') // 11
+function getInputType(e) {
+  const keyPressed = e.target
 
-function calculator (num1, operator, num2) {
+  if (keyPressed.classList.contains('number')) {
+    // debugger
+    firstNumberSet = keyPressed.value
+    console.log(firstNumberSet)
+    // calculator.num1 = firstNumberSet
+    captureDigits(keyPressed.value)
+  }
+
+  if (keyPressed.classList.contains('operation') && keyPressed.value !== '=') {
+    isOperatorPressed = true
+    // calculator.num1 = calculator.displayOutput
+    if (calculator.displayOutput !== '+') {
+      const beforeOperator = calculator.displayOutput
+      console.log(beforeOperator)
+
+    }
+    firstNumberSet = calculator.displayOutput
+    console.log(firstNumberSet)
+    operator = keyPressed.value
+    calculator.operator = keyPressed.value
+    console.log(calculator)
+    captureDigits(operator)
+    console.log(`${operator} operator pressed!`)
+    // calcDisplay.innerHTML = operator
+  }
+  renderScreen()
+}
+
+function captureDigits(numbers) {
+  console.log(numbers)
+  const displayOutput = calculator.displayOutput
+  // calculator.displayOutput = displayOutput === null ? numbers : displayOutput + numbers
+  // console.log(displayOutput)
+  const values = Object.values(calculator)
+  // calcDisplay.innerHTML = numbers + numbers
+  if (!displayOutput) {
+    calculator.displayOutput = numbers
+  } else {
+    calculator.displayOutput += numbers
+  }
+  console.log(values)
+}
+
+function calculate(num1, operator, num2) {
   let computedValue
 
   if (operator === '*') {
-    return
+
   } else if (operator === '/') {
-    return
+
   } else if (operator === '+') {
     computedValue = parseFloat(num1) + parseFloat(num2)
   } else if (operator === '-') {
-    return
+
   }
-  renderAnswer(computedValue)
+  renderScreen(computedValue)
 }
 
-function renderAnswer (answer) {
-  result.innerHTML = answer
+function renderScreen(data) {
+  calcDisplay.textContent = calculator.displayOutput
+  // calcDisplay.textContent = '0'
 }
