932976a7fb3da9d53d82ccf7dc468bd659c50bbf
Joe Bigio <jvbigio@gmail.com>
2020-09-18 18:15:00 -0400

    working on logic

--- app.js
+++ app.js
@@ -23,47 +23,53 @@ function getInputType (e) {
     firstOperand += keyPressed.value
     currentInput(keyPressed.value)
   } else if (keyPressed.classList.contains('number') && operation) {
-    console.log(operation) // + after pressing 1 + (2)
-    secondOperand += keyPressed.value
-    // currentInput(keyPressed.value) // original
-    calculator.displayOutput = ''
-    currentInput(secondOperand)
+    calculator.displayOutput = '' // test
 
-    // if (operation === '+') {
-    //   console.log(operation === '+') // true after pressing 2 in 1 + (2)
-    //   currentInput(secondOperand)
-    //   accumulatedValue = parseFloat(firstOperand) + parseFloat(secondOperand)
+    secondOperand += keyPressed.value
+    currentInput(keyPressed.value) // original
+    // new if statement
+    if (secondOperand) {
+      secondOperand = ''
+      secondOperand += keyPressed.value
+      accumulatedValue += parseFloat(secondOperand) // new
+      currentInput(accumulatedValue) // new
+    }
+    currentInput(accumulatedValue)
+
+    // if (accumulatedValue) {
+    calculator.displayOutput = ''
+    // currentInput(secondOperand) // orig
+    currentInput(keyPressed.value)
+    // currentInput(firstOperand) // keep, remove for test
     // }
-    // calculator.displayOutput = ''
   }
 
   if (keyPressed.classList.contains('operation') && keyPressed.value !== '=') {
     if (operation) {
       // continuous
-      // console.log(operation) // true after 1 + 2 (+)
-      // currentInput(keyPressed.value) // original
-
-      currentInput(secondOperand)
       if (operation === '+') {
-        // console.log(operation === '+') // true after 1 + 2 (+)
-        currentInput(secondOperand)
+        // original KEEP:
         accumulatedValue = parseFloat(firstOperand) + parseFloat(secondOperand)
+        // accumulatedValue += parseFloat(secondOperand)
+
+        calculator.displayOutput = ''
+        currentInput(accumulatedValue)
       }
     } else {
       // not continuos
-      // console.log(operation)
-      // currentInput(accumulatedValue) // keep
-      // calculator.displayOutput = ''
-      // currentInput(firstOperand)
+      console.log(operation)
+      currentInput(accumulatedValue)
+      calcDisplay.displayOutput = ''
       operation = keyPressed.value
-      // currentInput(keyPressed.value) // original shows => + sign
     }
   }
-
+  // = sign never pressed yet
   if (keyPressed.value === '=') {
     calculator.displayOutput = ''
+    // non-continuous only:
     calculate(firstOperand, operation, secondOperand)
-    console.log(accumulatedValue) // (38) => storedValue of 25 + 13
+    // accumulatedValue += parseFloat(secondOperand)
+    // currentInput(accumulatedValue) // doesn't belong here
   }
   renderScreen()
 }
