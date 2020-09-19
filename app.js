4225ed7ac8e2004c578153f5fdbb6b25ee013f0f
Joe Bigio <jvbigio@gmail.com>
2020-09-19 10:41:06 -0400

    refactored for guidance rqst

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-// const result = document.querySelector('.result')
-// document.addEventListener('DOMContentLoaded', function () {
-//   // renderScreen()
-//   // captureDigits()
-// })
-
 const calcDisplay = document.querySelector('.calculator-display')
 const calculator = { displayOutput: '' }
 let firstOperand = ''
 let secondOperand = ''
 let operation = ''
 let accumulatedValue = ''
-// let isOperatorPressed = false
 const buttons = document.querySelector('.buttons-container')
 buttons.addEventListener('click', getInputType)
 
@@ -23,51 +16,42 @@ function getInputType (e) {
     firstOperand += keyPressed.value
     currentInput(keyPressed.value)
   } else if (keyPressed.classList.contains('number') && operation) {
-    calculator.displayOutput = '' // test
+    calculator.displayOutput = ''
 
     secondOperand += keyPressed.value
-    currentInput(keyPressed.value) // original
-    // new if statement
+    currentInput(keyPressed.value)
     if (secondOperand) {
       secondOperand = ''
       secondOperand += keyPressed.value
-      accumulatedValue += parseFloat(secondOperand) // new
-      currentInput(accumulatedValue) // new
+      accumulatedValue += parseFloat(secondOperand)
+      currentInput(accumulatedValue)
     }
     currentInput(accumulatedValue)
 
-    // if (accumulatedValue) {
     calculator.displayOutput = ''
-    // currentInput(secondOperand) // orig
     currentInput(keyPressed.value)
-    // currentInput(firstOperand) // keep, remove for test
-    // }
   }
 
   if (keyPressed.classList.contains('operation') && keyPressed.value !== '=') {
-    if (operation) {
-      // continuous
-      if (operation === '+') {
-        // original KEEP:
-        accumulatedValue = parseFloat(firstOperand) + parseFloat(secondOperand)
-        // accumulatedValue += parseFloat(secondOperand)
+    // if (operation) { // Andy's code
+    // continuous
+    if (operation === '+') {
+      accumulatedValue = parseFloat(firstOperand) + parseFloat(secondOperand)
 
-        calculator.displayOutput = ''
-        currentInput(accumulatedValue)
-      }
-    } else {
+      calculator.displayOutput = ''
+      currentInput(accumulatedValue)
+      // }
+    } else { // Andy's code
       // not continuos
-      console.log(operation)
       currentInput(accumulatedValue)
       calcDisplay.displayOutput = ''
       operation = keyPressed.value
     }
   }
-  // = sign never pressed yet
   if (keyPressed.value === '=') {
     calculator.displayOutput = ''
-    // non-continuous only:
     calculate(firstOperand, operation, secondOperand)
+    accumulatedValue = parseFloat(firstOperand) + parseFloat(secondOperand)
   }
   renderScreen()
 }
@@ -82,12 +66,16 @@ function calculate (num1, operator, num2) {
 
   if (operator === '*') {
     computedValue = parseFloat(num1) * parseFloat(num2)
+    // return currentInput(computedValue)
   } else if (operator === '/') {
     computedValue = parseFloat(num1) / parseFloat(num2)
+    // return currentInput(computedValue)
   } else if (operator === '+') {
     computedValue = parseFloat(num1) + parseFloat(num2)
+    // return currentInput(computedValue)
   } else if (operator === '-') {
     computedValue = parseFloat(num1) - parseFloat(num2)
+    // return currentInput(computedValue)
   }
   currentInput(computedValue)
 }
