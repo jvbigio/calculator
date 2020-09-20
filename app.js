134f8a8cfd02af326d051734401251cdad7099dd
Joe Bigio <jvbigio@gmail.com>
2020-09-20 19:30:51 -0400

    working on test cases

--- app.js
+++ app.js
@@ -13,45 +13,57 @@ buttons.addEventListener('click', getInputType)
 // fix if statements
 
 function getInputType (e) {
+  debugger
   const keyPressed = e.target
   if (keyPressed.classList.contains('number') && !operation) {
     firstOperand += keyPressed.value
     currentInput(keyPressed.value)
   } else if (keyPressed.classList.contains('number') && operation) {
     calculator.displayOutput = ''
+    // calculator.displayOutput = firstOperand // new
     secondOperand += keyPressed.value
     currentInput(keyPressed.value)
     // if (secondOperand) {
     //   accumulatedValue += parseFloat(secondOperand)
     //   currentInput(accumulatedValue) // new
     // }
-    currentInput(accumulatedValue)
+    // currentInput(accumulatedValue)
 
     calculator.displayOutput = ''
-    currentInput(secondOperand)
+    currentInput(secondOperand) // keep, shows 2ndOp in: 1 + (3)
   }
 
   if (keyPressed.classList.contains('operation') && keyPressed.value !== '=') {
-    // if (operation) { // Andy's code
-    // continuous
+    if (operation) { // if in continuous operation
+      // continuous
+      // then inside of this use if op = +, -, *, /
+      if (operation === '+') {
+        calculator.displayOutput = '' // new
 
-    // if (operation === '+') {
-    if (operation === '+' && accumulatedValue === isNaN) { // new
-      accumulatedValue = parseFloat(firstOperand) + parseFloat(secondOperand)
-      // accumulatedValue += parseFloat(secondOperand)
-      calculator.displayOutput = ''
-      secondOperand = ''
-      currentInput(accumulatedValue)
-    } else if (operation === '+' && accumulatedValue !== isNaN) {
-      accumulatedValue += secondOperand
-      currentInput(accumulatedValue)
-    // }
+        currentInput(firstOperand)
+
+        firstOperand = parseFloat(firstOperand) + parseFloat(secondOperand) 
+        // accumulatedValue += parseFloat(secondOperand)
+        calculator.displayOutput = ''
+        secondOperand = ''
+        currentInput(firstOperand) // keep
+      }
+      if (operation === '-') {
+        // do math
+      }
+      if (operation === '*') {
+        // do math
+      }
+      if (operation === '/') {
+        // do math
+      }
     } else { // Andy's code
       // not continuous
-      currentInput(accumulatedValue)
+      // currentInput(accumulatedValue)
       calcDisplay.displayOutput = ''
       operation = keyPressed.value
-      accumulatedValue = parseFloat(firstOperand) + parseFloat(secondOperand)
+      // accumulatedValue = parseFloat(firstOperand) + parseFloat(secondOperand)
+      // calculate(firstOperand, operation, secondOperand) // 1NaN shows
     }
   }
   if (keyPressed.value === '=') {
@@ -72,14 +84,18 @@ function calculate (num1, operator, num2) {
 
   if (operator === '*') {
     computedValue = parseFloat(num1) * parseFloat(num2)
+    return currentInput(computedValue)
   } else if (operator === '/') {
     computedValue = parseFloat(num1) / parseFloat(num2)
+    return currentInput(computedValue)
   } else if (operator === '+') {
     computedValue = parseFloat(num1) + parseFloat(num2)
+    return currentInput(computedValue)
   } else if (operator === '-') {
     computedValue = parseFloat(num1) - parseFloat(num2)
+    return currentInput(computedValue)
   }
-  currentInput(computedValue)
+  currentInput(num2)
 }
 
 function renderScreen () {
