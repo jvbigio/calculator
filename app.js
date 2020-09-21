84f494ef80839543506b3aa1a3fef21a42622261
Joe Bigio <jvbigio@gmail.com>
2020-09-20 21:29:20 -0400

    working on decimal places

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ function getInputType (e) {
     // processOperationBtnPress()
 
     if (operation) { // if in continuous operation
-      // then inside of this use if op = +, -, *, /
       if (operation === '+') {
         calculator.displayOutput = ''
         currentInput(firstOperand)
@@ -36,13 +35,28 @@ function getInputType (e) {
         currentInput(firstOperand)
       }
       if (operation === '-') {
-        // do math
+        calculator.displayOutput = ''
+        currentInput(firstOperand)
+        firstOperand = parseFloat(firstOperand) - parseFloat(secondOperand)
+        calculator.displayOutput = ''
+        secondOperand = ''
+        currentInput(firstOperand)
       }
       if (operation === '*') {
-        // do math
+        calculator.displayOutput = ''
+        currentInput(firstOperand)
+        firstOperand = parseFloat(firstOperand) * parseFloat(secondOperand)
+        calculator.displayOutput = ''
+        secondOperand = ''
+        currentInput(firstOperand)
       }
       if (operation === '/') {
-        // do math
+        calculator.displayOutput = ''
+        currentInput(firstOperand)
+        firstOperand = parseFloat(firstOperand) / parseFloat(secondOperand)
+        calculator.displayOutput = ''
+        secondOperand = ''
+        currentInput(firstOperand)
       }
     } else { // not continuous
       calcDisplay.displayOutput = ''
@@ -102,7 +116,20 @@ function calculate (num1, operator, num2) {
     return currentInput(computedValue)
   } else if (operator === '/') {
     computedValue = parseFloat(num1) / parseFloat(num2)
-    return currentInput(computedValue)
+    // console.log(computedValue === Number.isInteger)
+    // // return (computedValue === Number.isInteger) ? currentInput(computedValue) : currentInput(computedValue.toFixed(3))
+    // if (computedValue === Number.isInteger) {
+    //   return currentInput(computedValue)
+    // } else {
+    //   return currentInput(computedValue.toFixed(3))
+    // }
+    // isInteger isn't the solution. find another way
+    // console.log(computedValue === Number.isInteger) // 4/3 false
+    // console.log(computedValue === Number.isInteger) // 5/3 false
+    console.log(computedValue === Number.isInteger) // 6/3 false
+    console.log(computedValue === Number.isInteger) // 6/2 false
+    // return currentInput(computedValue.toFixed(3))
+    return currentInput(computedValue) // original
   } else if (operator === '+') {
     computedValue = parseFloat(num1) + parseFloat(num2)
     return currentInput(computedValue)
@@ -116,3 +143,23 @@ function calculate (num1, operator, num2) {
 function renderScreen () {
   calcDisplay.textContent = calculator.displayOutput
 }
+
+// console.log(5 / 2) // 2.5
+// console.log(5 % 5) // 0 for no decimals
+// console.log(6 / 2) // 3
+// console.log(6 % 2) // 0
+// console.log(Number.isInteger(4 / 3)) // false
+// console.log(Number.isInteger(4 / 2)) // true
+// console.log(Number.isInteger(6 / 2)) // true
+// console.log(Number.isInteger(5 / 5)) // true
+
+// console.log(parseFloat(2.5).toFixed(3))
+
+/// modulus
+console.log(5 % 2) // 1
+console.log(6 % 5) // 1
+console.log(5 % 5) // 0
+console.log(6 % 2) // 0
+console.log(9 % 3) // 0
+console.log(9 % 4) // 1
+// so all decimals have modolus 1
